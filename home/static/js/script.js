40eb36deaa903984e144cd9be860fdf479779cfb
Sydeco <products.syde@gmail.com>
2023-05-15 15:49:04 +0700

    Update script.js

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -43,16 +43,19 @@ window.onKeyup = (e) => {
 
 document.onkeydown = (e) => {
     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U,A,P,F,S
-    if (event.keyCode === 123 ||
-    ctrlShiftKey(e, "I") ||
-    ctrlShiftKey(e, "J") ||
-    ctrlShiftKey(e, "C") ||
-    ctrlShiftKey(e, "M") ||
-    (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) ||
-    (e.ctrlKey && e.keyCode === "A".charCodeAt(0)) ||
-    (e.ctrlKey && e.keyCode === "P".charCodeAt(0)) ||
-    (e.ctrlKey && e.keyCode === "F".charCodeAt(0)) ||
-    (e.ctrlKey && e.keyCode === "S".charCodeAt(0)) || e.key == "PrintScreen") {
+    if (
+        event.keyCode === 123 ||
+        (e.ctrlShiftKey && e.keyCode === "I".charCodeAt(0)) ||
+        (e.ctrlShiftKey && e.keyCode === "J".charCodeAt(0)) ||
+        (e.ctrlShiftKey && e.keyCode === "C".charCodeAt(0)) ||
+        (e.ctrlShiftKey && e.keyCode === "M".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "U".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "A".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "P".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "F".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "S".charCodeAt(0)) ||
+        e.key == "PrintScreen"
+    ) {
         navigator.clipboard.writeText("");
     } else {
         debugger;
